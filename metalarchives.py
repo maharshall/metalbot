a29102aba0962c3d8c928d4de32243b014b91596
maharshall <maharshall@gmail.com>
2019-04-23 15:41:11 -0400

    backing up

--- metalarchives.py
+++ metalarchives.py
@@ -3,31 +3,39 @@
 import requests
 import datetime
 from bs4 import BeautifulSoup
+from selenium import webdriver
 import re
 import pprint
 
-def scrape_new_releases():
-    page = requests.get('https://metalinjection.net/category/new-music', headers={'User-Agent':'Mozilla/5.0'})
-    soup = BeautifulSoup(page.content, 'html.parser')
-    songs = []
+# def scrape_new_releases():
+browser = webdriver.Chrome('chromedriver.exe') 
+browser.get('https://www.metal-archives.com/release/upcoming')
+innerHTML = browser.execute_script("return document.body.innerHTML")
 
-    now = datetime.datetime.now()
-    today = str(now.day)
-    if len(today) == 1:
-        today = '0'+today
+# page = requests.get('https://www.metal-archives.com/release/upcoming', headers={'User-Agent':'Mozilla/5.0'})
+soup = BeautifulSoup(innerHTML, 'html.parser')
+songs = []
 
-    table = soup.find_all("tbody")
-    releases = table[0].find_all("tr")
-    for r in releases:
-        date = r.find_all("td")[4].text
-        day = re.findall('\d+', date.split(' ')[1])[0]
-        if len(day) == 1:
-            day = '0'+day
+pprint.pprint(soup)
+exit()
 
-        if day == today:
-            a = r.find_all("a")
-            songs.append([a[0].text, a[1].text])
-        else:
-            break
+now = datetime.datetime.now()
+today = str(now.day)
+if len(today) == 1:
+    today = '0'+today
 
-    return songs
+table = soup.find_all("tbody")
+releases = table[0].find_all("tr")
+for r in releases:
+    date = r.find_all("td")[4].text
+    day = re.findall('\d+', date.split(' ')[1])[0]
+    if len(day) == 1:
+        day = '0'+day
+
+    if day == today:
+        a = r.find_all("a")
+        songs.append([a[0].text, a[1].text])
+    else:
+        break
+
+    # return songs
